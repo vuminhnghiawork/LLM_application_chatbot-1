e1fc01ae3fae2d3ea7173e1a65e1ed70562904b3
nghiavm1 <nghia@gmail.com>
2024-12-02 09:52:07 +0700

    1st commit

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 from flask_cors import CORS		# newly added
 from flask import request
 import json
+import torch
 from transformers import AutoModelForSeq2SeqLM
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import BitsAndBytesConfig
+from transformers import AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline 
 
 app = Flask(__name__)
 CORS(app)				# newly added
@@ -11,9 +15,18 @@ CORS(app)				# newly added
 # model_name = "facebook/blenderbot-400M-distill"
 # model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 # tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+nf4_config = BitsAndBytesConfig(
+  load_in_4bit = True ,
+  bnb_4bit_quant_type = "nf4",
+  bnb_4bit_use_double_quant = True ,
+  bnb_4bit_compute_dtype = torch.bfloat16
+)
+
 model_name = "ricepaper/vi-gemma-2-2b-function-calling"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
+
+# model_name = 'microsoft/Phi-3-mini-4k-instructtokenizer'
+model = AutoModelForCausalLM.from_pretrained(model_name, quantization_config = nf4_config, low_cpu_mem_usage = True)
 conversation_history = []
 
 @app.route('/', methods = ['GET'])
@@ -34,6 +47,7 @@ def handle_prompt():
 
     # Tokenize the input text and history
     inputs = tokenizer.encode_plus(history, input_text, return_tensors="pt")
+
 
     # Generate the response from the model
     outputs = model.generate(**inputs, max_length= 255)  # max_length will acuse model to crash at some point as history grows
